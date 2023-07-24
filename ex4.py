15bddee2568ca3fee7ccedf187175f955f7c234e
Saglara <nohaevass@yandex.ru>
2023-07-24 19:16:11 +0300

    1

--- ex4.py
+++ ex4.py
@@ -2,6 +2,5 @@
 # ✔ Из последовательности исключите числа, сумма цифр которых равна 8.
 # ✔ Решение в одну строку.
 
-my_gen = (i for i in range(0, 101, 2) if i % 2 == 0)
-for char in my_gen:
-    print(char)
+
+print(*(i for i in range(0, 101, 2) if sum(map(int, str(i)))!=8))
