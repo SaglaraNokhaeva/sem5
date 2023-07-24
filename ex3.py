dd9c3ae7f9555baacf2000ad066162c824ba798e
Saglara <nohaevass@yandex.ru>
2023-07-24 18:38:03 +0300

    1

--- ex3.py
+++ ex3.py
@@ -3,3 +3,12 @@
 # Сохраните его итераторатор.
 # ✔ Далее выведите первые 5 пар ключ-значение,
 # обращаясь к итератору, а не к словарю.
+
+
+# 1 способ
+# str = input('Введите строку: ')
+# my_dictcomp = {str[i]: ord(str[i]) for i in range(len(str))}
+# print(my_dictcomp)
+
+# 2 способ
+print(my_dictcomp := {i: ord(i) for i in input('Введите строку: ')})
