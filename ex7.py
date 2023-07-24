71d3f2534d05c1687c2418e90f8c5eae4d8ff46b
Saglara <nohaevass@yandex.ru>
2023-07-24 21:26:59 +0300

    1

--- ex7.py
+++ ex7.py
@@ -3,11 +3,21 @@
 # ✔ Для проверки числа на простоту используйте правило: «число является простым, если делится
 # нацело только на единицу и на себя».
 
-number = int(input("Введите целое положительное число:"))
-def generator(number):
+
+
+def generator(n):
     count = 0
-    while count < number:
+    current = 2
+    while count < n:
+        flag = True
+        for i in range(2, current//2+1):
+            if not current % i:
+                flag = False
+        current += 1
+        if flag:
+            count += 1
+            yield current - 1
 
 
-
-
+generate = generator(int(input("Введите целое положительное число: ")))
+print(*generate)
