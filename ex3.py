c27d1055efa361716f1499e06204ffc07ae49ce8
Saglara <nohaevass@yandex.ru>
2023-07-24 19:02:20 +0300

    1

--- ex3.py
+++ ex3.py
@@ -3,11 +3,17 @@
 # ✔ Далее выведите первые 5 пар ключ-значение, обращаясь к итератору, а не к словарю.
 
 # 1 способ
+# my_str = input('Введите строку: ')
+# my_dictcomp = {my_str[i]: ord(my_str[i]) for i in range(len(my_str))}
+# print(my_dictcomp)
+# dict_iter = iter(my_dictcomp.items())
+# for i in range(5):
+#     print(*next(dict_iter))
+
+# 2 способ
 my_str = input('Введите строку: ')
 my_dictcomp = {my_str[i]: ord(my_str[i]) for i in range(len(my_str))}
 print(my_dictcomp)
 dict_iter = iter(my_dictcomp.items())
-for i in range(5):
-    print(*next(dict_iter))
-
+[print(*next(dict_iter)) for _ in range(5)]
 
